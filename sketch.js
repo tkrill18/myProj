94f575f9aea7e17146892ef9a270d8597c7ae45f
Thomas_ASC <tkrill18@gmail.com>
2017-06-29 15:03:34 -0400

    first commit

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup(){
+    createCanvas(600, 600);
+}
+
+//var x = 0;
+
+// Explore the reference section: p5js.org/reference
+function draw(){
+    background("#bae");
+    ellipse(mouseX, mouseY, 50);
+    // x = x + 1;
+    //x++;
+
+}
